1870b46a80e7528285b84a163230f63c66187eca
alenpaul2001 <alenpaul2001@gmail.com>
2022-05-19 19:43:29 +0530

    added examples

---
+++ examples/example.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:sbte/sbte.dart';
+
+Future<void> main(List<String> arguments) async {
+  print('Hello world!');
+  final username = input("Enter the username: ");
+  final password = input("Enter the password: ");
+  final sbte = SbteScrap(username: username, password: password);
+  final captchaImage = await sbte.getCaptcha();
+  var file = File("captcha.jpeg");
+  await file.writeAsBytes(captchaImage);
+  final solvedCaptcha = input("Enter the solved captcha: ");
+  JsonEncoder encoder = JsonEncoder.withIndent('  ');
+  await for (final sem in sbte.getExamResult(solvedCaptcha)) {
+    String prettyprint = encoder.convert(sem.toJson());
+    print(prettyprint);
+  }
+  sbte.close();
+}
+
+String input(String string) {
+  print(string);
+  return stdin.readLineSync()!;
+}
